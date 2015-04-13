c36c64be4f777f9368e3a35cadf6b200f175c4ae
Laura Barbera <laurabarbera@Lauras-Air.cable.rcn.com>
2015-04-13 04:02:25 -0400

    set up settings threads

--- Habitualizer/app/src/main/java/com/example/laurabarbera/habitualizer/Dashboard.java
+++ Habitualizer/app/src/main/java/com/example/laurabarbera/habitualizer/Dashboard.java
@@ -2,6 +2,7 @@ package com.example.laurabarbera.habitualizer;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -15,8 +16,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Dashboard extends ActionBarActivity {
+
+    private final int RESET_DISPLAY_LENGTH = 500;
+
+    String curName,
+           curQuestionSetting,
+           curLocationSetting,
+           curMotionSetting,
+           curBatterySetting;
+
+    final Context c = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,31 +40,34 @@ public class Dashboard extends ActionBarActivity {
 
         init();
 
-        final Context context = this;
-
         Button reset = (Button)findViewById(R.id.reset);
-        TextView name = (TextView)findViewById(R.id.confirmName);
-        UserProfile u = new UserProfile(getSharedPreferences("userProfile", MODE_PRIVATE));
-        String username = u.getName();
-        name.setText(username);
+
+//        UserProfile u = new UserProfile(getSharedPreferences("userProfile", MODE_PRIVATE));
+//        String username = u.getName();
+//        name.setText(username);
 
         reset.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
                 final Handler handler = new Handler();
                 Runnable r = new Runnable() {
                     @Override
                     public void run() {
                         handler.post(new Runnable() {
                             public void run() {
-                                new AlertDialog.Builder(context)
+                                new AlertDialog.Builder(c)
                                         .setMessage(R.string.error_reset)
                                         .setPositiveButton(R.string.error_yes, new DialogInterface.OnClickListener() {
                                             @Override
                                             public void onClick(DialogInterface dialog, int which) {
-                                                Intent returnBack = new Intent(Dashboard.this, FacebookLogin.class);
-                                                Dashboard.this.startActivity(returnBack);
-                                                Dashboard.this.finish();
+                                                new Handler().postDelayed(new Runnable() {
+                                                    // make it seem like it's deleting a lot of stuff by delaying for a moment
+                                                    @Override
+                                                    public void run() {
+                                                        Intent returnBack = new Intent(Dashboard.this, FacebookLogin.class);
+                                                        Dashboard.this.startActivity(returnBack);
+                                                        Dashboard.this.finish();
+                                                    }
+                                                }, RESET_DISPLAY_LENGTH);
                                             }
                                         })
                                         .setNegativeButton(R.string.error_no, null)
@@ -63,26 +80,89 @@ public class Dashboard extends ActionBarActivity {
                 Thread t = new Thread(r);
                 t.start();
 
-                SaveData save = new SaveData();
-                save.execute();
+                ResetData resetData = new ResetData();
+                resetData.execute();
             }
         });
-    }
-    private class SaveData extends AsyncTask<String, Void, Boolean> {
 
+        final Handler handler = new Handler();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                handler.post(new Runnable(){
+                    public void run() {
+
+                    }
+                });
+            }
+        };
+        Thread t1 = new Thread(r);
+        t1.start();
+        LoadPrefs loadPrefs = new LoadPrefs();
+        loadPrefs.execute();
+    }
+    private class LoadPrefs extends AsyncTask<String, Void, Boolean> {
         @Override
         protected Boolean doInBackground(String... params) {
-
-            SharedPreferences userProfile = getSharedPreferences("userProfile", Activity.MODE_PRIVATE);
-            SharedPreferences.Editor editor = userProfile.edit();
-            editor.putString("name",null);
-            editor.commit();
-
+            UserProfile u = new UserProfile(getSharedPreferences("userProfile", MODE_PRIVATE));
+            curName = u.getName();
+            curBatterySetting = u.getBatterySetting(c);
+            curLocationSetting = u.getLocation(c);
+            curMotionSetting = u.getMotionSetting(c);
+            curQuestionSetting = u.getQuestionLevel(c);
             return true;
         }
         protected void onPostExecute(Boolean result) {
+            TextView name = (TextView)findViewById(R.id.confirmName);
+            TextView questionSetting = (TextView)findViewById(R.id.notificationSetting);
+            TextView locationSetting = (TextView)findViewById(R.id.locationSetting);
+            TextView motionSetting = (TextView)findViewById(R.id.motionSetting);
+            TextView batterySetting = (TextView)findViewById(R.id.batterySetting);
+            name.setText(curName);
+            questionSetting.setText(curQuestionSetting);
+            locationSetting.setText(curLocationSetting);
+            motionSetting.setText(curMotionSetting);
+            batterySetting.setText(curBatterySetting);
         }
     }
+    private class ResetData extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            UserProfile u = new UserProfile(getSharedPreferences("userProfile", MODE_PRIVATE));
+            u.setName(null);
+
+            return true;
+        }
+        protected void onPostExecute(Boolean result) {}
+    }
+    private class UpdateName extends AsyncTask<String, Void, Boolean> {
+        public UpdateName(String newName) {
+            this.newName = newName;
+        }
+        @Override
+        protected Boolean doInBackground(String... params) {
+            UserProfile u = new UserProfile(getSharedPreferences("userProfile", MODE_PRIVATE));
+            u.setName(this.newName);
+            return true;
+        }
+        protected void onPostExecute(Boolean result) {}
+        private String newName;
+    }
+    private class UpdateQuestions extends AsyncTask<String, Void, Boolean> {
+        public UpdateQuestions(int questionLevel) {
+            this.questionLevel = questionLevel;
+        }
+        @Override
+        protected Boolean doInBackground(String... params) {
+            UserProfile u = new UserProfile(getSharedPreferences("userProfile", MODE_PRIVATE));
+            u.setQuestionLevel(this.questionLevel);
+            return true;
+        }
+        protected void onPostExecute(Boolean result) {}
+        private int questionLevel;
+    }
+
     public void init() {
         ActionBar a = getSupportActionBar();
         setTitle(R.string.getstarted);
--- Habitualizer/app/src/main/java/com/example/laurabarbera/habitualizer/UserProfile.java
+++ Habitualizer/app/src/main/java/com/example/laurabarbera/habitualizer/UserProfile.java
@@ -1,5 +1,6 @@
 package com.example.laurabarbera.habitualizer;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 
 /**
@@ -10,14 +11,80 @@ public class UserProfile {
         super();
         this.shared = s;
     }
-    public String getName() {
-        String n = shared.getString("name","");
-        return n;
-    }
     public void setName(final String n) {
         SharedPreferences.Editor editor = shared.edit();
-        editor.putString("name",n);
+        editor.putString("name", n);
         editor.commit();
+    }
+    public void setQuestionLevel(final int questionLevel) {
+        SharedPreferences.Editor editor = shared.edit();
+        editor.putInt("question level", questionLevel);
+    }
+    public void setLocation(final int location) {
+        SharedPreferences.Editor editor = shared.edit();
+        editor.putInt("location", location);
+    }
+    public void setMotion(final int motion) {
+        SharedPreferences.Editor editor = shared.edit();
+        editor.putInt("motion", motion);
+    }
+    public void setPerformancelevel(final int performanceLevel) {
+        SharedPreferences.Editor editor = shared.edit();
+        editor.putInt("performance level", performanceLevel);
+    }
+    public String getName() {
+        String n = shared.getString("name", "");
+        return n;
+    }
+    public String getQuestionLevel(Context c) {
+        String s;
+        int n = shared.getInt("question level",0);
+        if ( n == 1 ) {
+            s = c.getString(R.string.notificationSetting_low);
+        }
+        else if ( n == 2 ) {
+            s = c.getString(R.string.notificationSetting_medium);
+        }
+        else {
+            s = c.getString(R.string.notificationSetting_high);
+        }
+        return s;
+    }
+    public String getLocation(Context c) {
+        String s;
+        int n = shared.getInt("location",0);
+        if ( n == 1 ) {
+            s = c.getString(R.string.locationSetting_on);
+        }
+        else {
+            s = c.getString(R.string.locationSetting_off);
+        }
+        return s;
+    }
+    public String getBatterySetting(Context c) {
+        String s;
+        int n = shared.getInt("performance level",0);
+        if ( n == 1 ) {
+            s = c.getString(R.string.batterySetting_low);
+        }
+        else if ( n == 2 ) {
+            s = c.getString(R.string.batterySetting_medium);
+        }
+        else {
+            s = c.getString(R.string.batterySetting_high);
+        }
+        return s;
+    }
+    public String getMotionSetting(Context c) {
+        String s;
+        int n = shared.getInt("motion",0);
+        if ( n == 0 ) {
+            s = c.getString(R.string.motionSetting_off);
+        }
+        else {
+            s = c.getString(R.string.motionSetting_on);
+        }
+        return s;
     }
     private SharedPreferences shared;
 }
